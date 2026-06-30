65c81954fe3c497a8867dac216f1564e0d52ce8d
imx <phtuanh.imx@gmail.com>
2026-07-01 01:23:57 +0700

    AdsKit: initial release (SwiftUI/async-await iOS base)

---
+++ Tests/AdsCoreTests/AdsCoreTests.swift
@@ -0,0 +1,25 @@
+//
+//  AdsCoreTests.swift
+//  AdsKit
+//
+//  Created by AnhPT on 02/07/2026.
+//
+
+import XCTest
+@testable import AdsCore
+
+@MainActor
+final class AdsCoreTests: XCTestCase {
+    func testTestConfigHasUnits() {
+        XCTAssertNotNil(AdConfiguration.test.bannerID)
+        XCTAssertNotNil(AdConfiguration.test.interstitialID)
+    }
+
+    func testNullManagerNeverShows() async {
+        let manager = NullAdsManager()
+        let shown = await manager.showInterstitial()
+        XCTAssertFalse(shown)
+        let reward = await manager.showRewarded()
+        XCTAssertNil(reward)
+    }
+}
